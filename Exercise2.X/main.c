f7a02757dd692e3d5214854e8b8d70e11825f126
Frank Lu <frankz.lu@googlemail.com>
2018-11-19 18:18:36 +0100

    ex2 setup

---
+++ Exercise2.X/main.c
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.c
+ * Author: frankzl
+ *
+ * Created on November 19, 2018, 6:09 PM
+ */
+
+#include <p30Fxxxx.h>
+
+// FOSC
+#pragma config FPR = XT_PLL8            // Primary Oscillator Mode (XT w/PLL 8x)
+#pragma config FOS = PRI                // Oscillator Source (Primary Oscillator)
+#pragma config FCKSMEN = CSW_FSCM_OFF   // Clock Switching and Monitor (Sw Disabled, Mon Disabled)
+
+// FWDT
+#pragma config FWPSB = WDTPSB_16        // WDT Prescaler B (1:16)
+#pragma config FWPSA = WDTPSA_512       // WDT Prescaler A (1:512)
+#pragma config WDT = WDT_OFF            // Watchdog Timer (Disabled)
+
+// FBORPOR
+#pragma config FPWRT = PWRT_64          // POR Timer Value (64ms)
+#pragma config BODENV = BORV20          // Brown Out Voltage (Reserved)
+#pragma config BOREN = PBOR_OFF         // PBOR Enable (Disabled)
+#pragma config LPOL = PWMxL_ACT_HI      // Low-side PWM Output Polarity (Active High)
+#pragma config HPOL = PWMxH_ACT_LO      // High-side PWM Output Polarity (Active Low)
+#pragma config PWMPIN = RST_PWMPIN      // PWM Output Pin Reset (Control with HPOL/LPOL bits)
+#pragma config MCLRE = MCLR_EN          // Master Clear Enable (Enabled)
+
+// FGS
+#pragma config GWRP = GWRP_OFF          // General Code Segment Write Protect (Disabled)
+#pragma config GCP = CODE_PROT_OFF      // General Segment Code Protection (Disabled)
+
+// FICD
+#pragma config ICS = ICS_PGD            // Comm Channel Select (Use PGC/EMUC and PGD/EMUD)
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    unsigned int i = 0;
+    while(1){
+        if (i >= 17){
+            i = 0;
+        }else{
+            i++;
+        }
+    }
+}
+
